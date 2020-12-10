e1304631ec37cf5fe8fe57bee0f2a8ff13545b77
Nicolas Malgat <nicolas.malgat@hotmail.com>
2020-12-11 00:48:19 +0100

    Limitation du nombre de resultats a chaque reauete a 10 maximum

--- fantoir/src/main/java/com/fantoir/model/AdressRepository.java
+++ fantoir/src/main/java/com/fantoir/model/AdressRepository.java
@@ -8,19 +8,19 @@ import org.springframework.data.repository.query.Param;
 
 public interface AdressRepository extends CrudRepository<Adresse, String> {
 	
-    @Query(value = "select * from Adresse where id_voie = :idVoie", nativeQuery = true)
+    @Query(value = "select * from Adresse where id_voie = :idVoie LIMIT 10", nativeQuery = true)
     Adresse findByIdVoie(@Param("idVoie") String idVoie);
 
-    @Query(value = "select * from Adresse where num_voie = :numVoie", nativeQuery = true)
+    @Query(value = "select * from Adresse where num_voie = :numVoie  LIMIT 10", nativeQuery = true)
     List<Adresse> findByNumVoie(@Param("numVoie") int numVoie);
 
-    @Query(value = "select * from Adresse where num_voie = :numVoie and nom_voie = :nomVoie", nativeQuery = true)
+    @Query(value = "select * from Adresse where num_voie = :numVoie and nom_voie = :nomVoie  LIMIT 10", nativeQuery = true)
     List<Adresse> findAdressByVoie(@Param("numVoie") int numVoie, @Param("nomVoie") String nomVoie);
 
-    @Query(value = "select * from Adresse where num_voie = :numVoie and nom_voie = :nomVoie and nom_commune = :nomCommune", nativeQuery = true)
+    @Query(value = "select * from Adresse where num_voie = :numVoie and nom_voie = :nomVoie and nom_commune = :nomCommune  LIMIT 10", nativeQuery = true)
     List<Adresse> findAdressByComm(@Param("numVoie") int numVoie, @Param("nomVoie") String nomVoie, @Param("nomCommune") String nomCommune);
 
-    @Query(value = "select * from Adresse where num_voie = :numVoie and nom_voie = :nomVoie and nom_commune = :nomCommune and cod_postal = :codPostal", nativeQuery = true)
+    @Query(value = "select * from Adresse where num_voie = :numVoie and nom_voie = :nomVoie and nom_commune = :nomCommune and cod_postal = :codPostal  LIMIT 10", nativeQuery = true)
     List<Adresse> findAdressByCod(@Param("numVoie") int numVoie, @Param("nomVoie") String nomVoie, @Param("nomCommune") String nomCommune, @Param("codPostal") int codPostal);
 
 }
